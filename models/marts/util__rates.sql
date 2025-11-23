8d1912d68abb433066b6ace2654dfaffa13b0c74
pleskop <peter@evidy.cz>
2025-11-23 11:03:10 +0100

    feat(rates): add support for GBP and PLN in exchange rates calculations
    Extend the exchange rates models to include GBP and PLN currencies. Update calculations to handle these currencies and increase rounding precision to 5 decimal places for improved accuracy.

--- models/marts/util__rates.sql
+++ models/marts/util__rates.sql
@@ -8,18 +8,26 @@ final as (
 
     select
         date_day,
-        round(usd, 4) as czk_to_usd,
-        round(eur, 4) as czk_to_eur,
-        round(huf, 4) as czk_to_huf,
-        round(1 / usd, 4) as usd_to_czk,
-        round(1 / eur, 4) as eur_to_czk,
-        round(1 / huf, 4) as huf_to_czk,
-        round(eur / usd, 4) as usd_to_eur,
-        round(usd / eur, 4) as eur_to_usd,
-        round(huf / usd, 4) as usd_to_huf,
-        round(usd / huf, 4) as huf_to_usd,
-        round(huf / eur, 4) as eur_to_huf,
-        round(eur / huf, 4) as huf_to_eur
+        round(usd, 5) as czk_to_usd,
+        round(eur, 5) as czk_to_eur,
+        round(huf, 5) as czk_to_huf,
+        round(pln, 5) as czk_to_pln,
+        round(gbp, 5) as czk_to_gbp,
+        round(1 / usd, 5) as usd_to_czk,
+        round(1 / eur, 5) as eur_to_czk,
+        round(1 / huf, 5) as huf_to_czk,
+        round(1 / pln, 5) as pln_to_czk,
+        round(1 / gbp, 5) as gbp_to_czk,
+        round(eur / usd, 5) as usd_to_eur,
+        round(usd / eur, 5) as eur_to_usd,
+        round(huf / usd, 5) as usd_to_huf,
+        round(usd / huf, 5) as huf_to_usd,
+        round(huf / eur, 5) as eur_to_huf,
+        round(eur / huf, 5) as huf_to_eur,
+        round(eur / pln, 5) as pln_to_eur,
+        round(pln / eur, 5) as eur_to_pln,
+        round(eur / gbp, 5) as gbp_to_eur,
+        round(gbp / eur, 5) as eur_to_gbp,
 
     from source
 )
